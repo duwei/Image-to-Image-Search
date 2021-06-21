6b0ac007bf6cc4c6c4c115cddcb2650c7be16746
DuWei <duweiwork@gmail.com>
2021-06-21 11:02:10 +0900

    sift comparison

--- server.py
+++ server.py
@@ -106,16 +106,18 @@ def search():
         ids = np.argsort(dists)[:30]
         answers = [(img_paths[id], dists[id]) for id in ids]
 
-        kp1, des1 = sift.detectAndCompute(cv2.imread(uploaded_img_path, 0), None)  # 提取比对图片的特征
+        img1 = cv2.imread(uploaded_img_path, 0)
+        kp1, des1 = sift.detectAndCompute(img1, None)  # 提取比对图片的特征
         sift_answers = []
         images = glob.glob(os.path.join(app.config['UPLOAD_FOLDER'], '*'))
         for image in images:
-            kp2, des2 = sift.detectAndCompute(cv2.imread(image, 0), None)
+            img2 = cv2.imread(image, 0)
+            kp2, des2 = sift.detectAndCompute(img2, None)
             matches = matcher.knnMatch(des1, des2, k=2)  # 匹配特征点，为了删选匹配点，指定k为2，这样对样本图的每个特征点，返回两个匹配
             (match_num, matches_mask) = get_match_num(matches, 0.9)  # 通过比率条件，计算出匹配程度
             match_ratio = match_num * 100 / len(matches)
             draw_params = dict(matchColor=(0, 255, 0), singlePointColor=(255, 0, 0), matchesMask=matches_mask, flags=0)
-            comparison_image = cv2.drawMatchesKnn(uploaded_img_path, kp1, image, kp2, matches, None, **draw_params)
+            comparison_image = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
             tmp_file_name = "./static/tmp/" + uuid.uuid4().hex
             cv2.imwrite(tmp_file_name, comparison_image)
             sift_answers.append((tmp_file_name, match_ratio))
