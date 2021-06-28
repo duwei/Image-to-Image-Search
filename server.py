9e24514983c4c700b296e6b80e5b3114955605a4
DuWei <duweiwork@gmail.com>
2021-06-28 12:36:56 +0900

    sort by match num

--- server.py
+++ server.py
@@ -196,7 +196,7 @@ def sift_search():
                 (match_num, matches_mask) = get_match_num(matches, 0.9)  # 通过比率条件，计算出匹配程度
                 match_ratio = match_num * 100 / len(matches)
                 # answers.append((Path("./static/database") / r.get(key), round(match_ratio, 2)))
-                answers.append((Path("./static/database") / r.get(key), match_num))
+                answers.append((Path("./static/database") / ('f_' + r.get(key)), match_num))
 
         answers.sort(key=lambda x: x[1], reverse=True)  # 按照匹配度排序
         good = [x for x in answers if x[1] > 50]
@@ -289,6 +289,36 @@ def upload():
 
                 file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 file.save(file_path)
+
+                img = cv2.imread(filename)
+                img = cv2.resize(img, (224, 224))
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+                # blob 검출 필터 파라미터 생성 ---①
+                params = cv2.SimpleBlobDetector_Params()
+
+                # 경계값 조정 ---②
+                params.minThreshold = 10
+                params.maxThreshold = 240
+                params.thresholdStep = 5
+                # 면적 필터 켜고 최소 값 지정 ---③
+                params.filterByArea = True
+                params.minArea = 200
+
+                # 컬러, 볼록 비율, 원형비율 필터 옵션 끄기 ---④
+                params.filterByColor = False
+                params.filterByConvexity = False
+                params.filterByInertia = False
+                params.filterByCircularity = False
+
+                # 필터 파라미터로 blob 검출기 생성 ---⑤
+                detector = cv2.SimpleBlobDetector_create(params)
+                # 키 포인트 검출 ---⑥
+                keypoints = detector.detect(gray)
+                # 키 포인트 그리기 ---⑦
+                img_draw = cv2.drawKeypoints(img, keypoints, None, None, cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                # 결과 출력 ---⑧
+                cv2.imwrite('f_' + filename, img_draw)
 
                 feature = fe.extract(img=Image.open(file_path))
                 feature_path = Path("./static/feature") / (filename + ".npy")  # e.g., ./static/feature/xxx.npy
