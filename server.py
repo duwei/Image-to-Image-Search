9092151183f2586878f3876219017d529c30db28
DuWei <duweiwork@gmail.com>
2021-07-08 16:01:11 +0900

    add yolov5

--- server.py
+++ server.py
@@ -274,10 +274,10 @@ def template_search():
             if w2 <= w or h2 <= h:
                 continue
             img = img2.copy()
-            res = cv2.matchTemplate(img, img1, cv2.TM_CCOEFF_NORMED)
+            res = cv2.matchTemplate(img, img1, cv2.TM_CCORR_NORMED)
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-            # if min_val < 0.8:
-            #     continue
+            if min_val < 0.8:
+                continue
             top_left = max_loc
             bottom_right = (top_left[0] + w, top_left[1] + h)
             cv2.rectangle(img, top_left, bottom_right, 255, 2)
